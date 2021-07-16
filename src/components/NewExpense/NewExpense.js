15e845a747848e7c96dd705f394b9605632de1bb
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-16 23:58:40 +0200

    toggle

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = ({ onAddExpense }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,9 +12,26 @@ const NewExpense = ({ onAddExpense }) => {
     };
     onAddExpense(expenseData);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const cancelEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={cancelEditingHandler}
+        />
+      )}
     </div>
   );
 };
