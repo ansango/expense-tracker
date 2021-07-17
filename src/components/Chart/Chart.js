34d2082bb4b0e39462f29e387f02723e9cc602a5
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-17 03:12:14 +0200

    chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,11 +1,10 @@
-import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 const Chart = ({ dataPoints }) => {
   const dataPointValues = dataPoints.map((dataPoint) => dataPoint.value);
   const totalMax = Math.max(...dataPointValues);
   return (
-    <div className="chart">
+    <div className="p-4 rounded-xl bg-red-300 text-center flex justify-between h-40">
       {dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,18 +1,18 @@
-import "./ChartBar.css";
-
 const ChartBar = ({ value, maxValue, label }) => {
   const roundedValue = Math.round((value / maxValue) * 100) + "%";
   const barFillHeight = maxValue > 0 ? roundedValue : "0%";
 
   return (
-    <div className="chart-bar">
-      <div className="chart-bar__inner">
+    <div className="flex flex-col h-full items-center">
+      <div className="h-full w-full border-2 border-red-500 rounded-lg overflow-hidden flex flex-col justify-end">
         <div
-          className="chart-bar__fill"
+          className="bg-red-500 w-full transition-all duration-1000 ease-in-out"
           style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="chart-bar__label">{label}</div>
+      <div className="text-center text-red-900" style={{ fontSize: "0.6rem" }}>
+        {label}
+      </div>
     </div>
   );
 };
